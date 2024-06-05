a46406e15019910be1b6c76243fda5ec4ae901de
natia02 <natia2.pruidze@outlook.com>
2024-06-05 13:41:04 +0400

    change return types of methods to return true or false

--- Phone Book Application/PhoneBook.cs
+++ Phone Book Application/PhoneBook.cs
@@ -5,8 +5,7 @@ namespace Phone_Book_Application;
 public class PhoneBook
 {
     private Dictionary<string, Contact> Contacts { get; }
-    private const string FileName = @"C:\Users\Natia\RiderProjects\Phone Book Application\Phone Book Application\Data\contacts.json";
-    private static readonly JsonSerializerOptions Options = new JsonSerializerOptions { WriteIndented = true };
+    private static readonly string FileName = Path.Combine(GetProjectDirectory(), "Data", "contacts.json");
         
 
     
@@ -15,33 +14,48 @@ public class PhoneBook
         Contacts = new Dictionary<string, Contact>();
         LoadContacts();
     }
+    
+    private static string GetProjectDirectory()
+    {
+        var currentDirectory = AppContext.BaseDirectory;
+        var projectDirectory = Directory.GetParent(currentDirectory)?.Parent?.Parent?.Parent?.FullName;
+        if (projectDirectory == null)
+        {
+            throw new DirectoryNotFoundException("Project directory not found.");
+        }
+        return projectDirectory;
+    }
 
-    public void AddContact(string name, string phoneNumber)
+    public bool AddContact(string name, string phoneNumber)
     {
         if (!Contacts.ContainsKey(name))
         {
             Contact contact = new Contact(name, phoneNumber);
             Contacts.Add(name, contact);
             Console.WriteLine($"Contact with name {name} has been added.");
+            return true;
         }
         else
         {
             Console.WriteLine($"Contact with name {name} already exists.");
+            return false;
         }
         
         
     }
     
-    public void RemoveContact(string name)
+    public bool RemoveContact(string name)
     {
         if (Contacts.ContainsKey(name))
         {
             Contacts.Remove(name);
             Console.WriteLine($"Contact with name {name} has been removed.");
+            return true;
         }
         else
         {
             Console.WriteLine($"Contact with name {name} does not exist.");
+            return false;
         }
         
     }
@@ -62,36 +76,40 @@ public class PhoneBook
         }
     }
 
-    public void SearchByName(string name)
+    public bool SearchByName(string name)
     {
         if (Contacts.TryGetValue(name, out var contact))
         {
             Console.WriteLine($"Name: {name} Phone number: {contact.PhoneNumber}");
+            return true;
         }
         else
         {
             Console.WriteLine($"Contact with name {name} does not exist.");
+            return false;
         }
     }
 
-    public void UpdateContact(string name, string phoneNumber)
+    public bool UpdateContact(string name, string phoneNumber)
     {
         if (Contacts.TryGetValue(name, out var contact))
         {
             Contacts[name] = contact with {PhoneNumber = phoneNumber};
             Console.WriteLine($"Contact with name {name} has been updated.");
+            return true;
         }
         else
         {
             AddContact(name, phoneNumber);
             Console.WriteLine($"Old contact with name {name} does not exist. new contact is created.");
+            return false;
         }
         
     }
     
   
 
-    public void SaveContacts()
+    public bool SaveContacts()
     {
         try
         {
@@ -101,19 +119,21 @@ public class PhoneBook
                 Directory.CreateDirectory(directory);
             }
             
-            var json = JsonSerializer.Serialize(Contacts, Options);
+            var json = Contacts.ToJson(writeIndented : true);
             File.WriteAllText(FileName, json);
             Console.WriteLine("Contacts have been saved.");
+            return true;
 
         }
         catch (Exception e)
         {
             Console.WriteLine($"Error saving contacts: {e.Message}");
+            return false;
         }
         
     }
     
-    private void LoadContacts()
+    private bool LoadContacts()
     {
         try
         {
@@ -128,6 +148,7 @@ public class PhoneBook
                         Contacts.Add(contact.Key, contact.Value);
                     }
                     Console.WriteLine("Contacts loaded successfully.");
+                    return true;
                 }
             }
 
@@ -135,7 +156,9 @@ public class PhoneBook
         catch (Exception e)
         {
             Console.WriteLine($"Error loading contacts: {e.Message}");
+            return false;
         }
+        return false;
         
     }
     
